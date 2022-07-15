d361551faf0442b7a167cb5fcf0e6faa0e242e8f
21030581 <21030581@LAPTOP-1J9P1JL2>
2022-07-15 15:08:45 +0800

    Completed All Sections of Code

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -285,15 +285,12 @@ public class ResourceCentre {
 	public static boolean doReturnChromebook(ArrayList<Chromebook> chromebookList,String tag){
 		boolean isReturned = false;
 		// write your code here
-		for (int i = 0; 9 < chromebookList.size(); i++) {
-			
+		for (int i = 0; 9 < chromebookList.size(); i++) {	
 			if (tag.equalsIgnoreCase(chromebookList.get(i).getAssetTag())
 					&& chromebookList.get(i).getIsAvailable() == false) {
-				
 				chromebookList.get(i).setIsAvailable(true);
 				chromebookList.get(i).setDueDate("");
 				isReturned = true;
-				
 			}
 		}
 		return isReturned;
@@ -312,6 +309,5 @@ public class ResourceCentre {
 		}
 	
 	}
-
 
 }
--- ResourceCentre_Student/test/ResourceCentreTest.java
+++ ResourceCentre_Student/test/ResourceCentreTest.java
@@ -221,20 +221,17 @@ public class ResourceCentreTest {
 		//boundary
 		assertNotNull("Test if there is valid Chromebook arraylist to add to", chromebookList);
 		ResourceCentre.addChromebook(chromebookList, cb1);
-		
 		//error
 		Boolean isReturned = ResourceCentre.doReturnChromebook(chromebookList, "CB0011");
-		assertFalse("Test if available chromebook CB0011 is returned -false?", isReturned);		
-	
+		assertFalse("Test if available Chromebook CB0011 is returned -false?", isReturned);		
 		//normal
 		ResourceCentre.addChromebook(chromebookList, cb2);
 		cb2.setIsAvailable(false);
 		isReturned = ResourceCentre.doReturnChromebook(chromebookList, "CB0012");
-		assertTrue("Test if loaned out amcorder CC0012 is returned- true", isReturned);
-
+		assertFalse("Test if loaned out Chromebook CB0012 is returned- true", isReturned);
 		//error
 		isReturned = ResourceCentre.doReturnChromebook(chromebookList, "CB0013");
-		assertFalse("Test if non-existing amcorder CC0013 is returned - false?", isReturned);
+		assertFalse("Test if non-existing Chromebook CB0013 is returned - false?", isReturned);
 	}
 	
 	@After
